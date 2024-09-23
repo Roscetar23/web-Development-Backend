7541e21ea4119ba2b5edd9617614ece162e1bec2
David Leal <adavidlealf@gmail.com>
2024-09-22 21:02:22 -0500

    Ejemplo de service y controller de Material
    Falta implementar la conexion con la base de datos

--- src/modules/product/material/material.controller.ts
+++ src/modules/product/material/material.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { MaterialService } from './material.service';
+import { Material } from './material.entity';
 
 @Controller('material')
-export class MaterialController {}
+export class MaterialController {
+
+    constructor(private readonly materialService : MaterialService){}
+
+    @Get()
+    getAllMaterials() : Material[] {
+        return this.materialService.getMaterials();
+    }
+
+    @Get(':id')
+    getMaterialById(@Param('id') id : string) : Material {
+        return this.materialService.getMaterial(parseInt(id));
+    }
+
+    @Post()
+    createMaterial(@Body() body) : void {
+        this.materialService.createMaterial(body.name, body.price);
+    }
+
+    @Patch(':id')
+    updateMaterial(@Param('id') id : string, @Body() body) : Material {
+        return this.materialService.updateMaterial(parseInt(id), body.name, body.price);
+    }
+
+    @Delete(':id')
+    removeMaterial(@Param('id') id : string) : void {
+        this.materialService.removeMaterial(parseInt(id));
+    }
+}
